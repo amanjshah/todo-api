2022f23b84e0246fc1536b072c6577636fa25518
Aman Shah <aman.j.shah@gmail.com>
2024-01-21 13:59:43 +0000

    disable authentication for options requests

--- src/main/java/com/aman/rest/authentication/BasicAuthSecurityConfig.java
+++ src/main/java/com/aman/rest/authentication/BasicAuthSecurityConfig.java
@@ -2,6 +2,7 @@ package com.aman.rest.authentication;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,7 +14,11 @@ public class BasicAuthSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .authorizeHttpRequests(auth -> auth
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest()
+                .authenticated()
+            )
             .httpBasic(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .csrf().disable()
