5ca9f623412efb4360d943311d96caf0c47b0704
Aman Shah <aman.j.shah@gmail.com>
2024-01-26 11:30:36 +0000

    add data to to-do table in h2 console upon its initialisation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, is_done, target_date, username)
+values (1001, 'Learn AWS', false, CURRENT_DATE(), 'aman');
+
+insert into todo (id, description, is_done, target_date, username)
+values (1002, 'Finish Spring Boot project', false, CURRENT_DATE(), 'aman');
+
+insert into todo (id, description, is_done, target_date, username)
+values (1003, 'Finish reading UDS', false, CURRENT_DATE(), 'aman');
