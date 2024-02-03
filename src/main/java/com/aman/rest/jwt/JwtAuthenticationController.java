8342456704b417d11c194c362194f3a630f46f5b
Aman Shah <aman.j.shah@gmail.com>
2024-02-03 16:50:42 +0000

    simplify auth token generation logic

--- src/main/java/com/aman/rest/jwt/JwtAuthenticationController.java
+++ src/main/java/com/aman/rest/jwt/JwtAuthenticationController.java
@@ -1,9 +1,18 @@
 package com.aman.rest.jwt;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class JwtAuthenticationController {
 
-    private final JwtTokenService tokenService;
+    private final JwtEncoder jwtEncoder;
     private final AuthenticationManager authenticationManager;
 
     @PostMapping("/authenticate")
@@ -27,9 +36,32 @@ public class JwtAuthenticationController {
         var authentication =
             authenticationManager.authenticate(authenticationToken);
 
-        var token = tokenService.generateToken(authentication);
+        var token = generateToken(authentication);
 
         return ResponseEntity.ok(new JwtTokenResponse(token));
+    }
+
+    private String generateToken(Authentication authentication) {
+
+        var claims = JwtClaimsSet.builder()
+            .issuer("self")
+            .issuedAt(Instant.now())
+            .expiresAt(Instant.now().plus(90, ChronoUnit.MINUTES))
+            .subject(authentication.getName())
+            .claim("scope", generateScope(authentication))
+            .build();
+
+        return jwtEncoder
+            .encode(JwtEncoderParameters.from(claims))
+            .getTokenValue();
+    }
+
+    private static String generateScope(Authentication authentication) {
+        return authentication
+            .getAuthorities()
+            .stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.joining(" "));
     }
 
     public record JwtTokenRequest(String username, String password) {}
