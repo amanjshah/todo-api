c5f5c13e07e2f990b9f1762121a8921c5b7e950e
Aman Shah <aman.j.shah@gmail.com>
2024-02-04 14:44:54 +0000

    add a user DAO

---
+++ src/main/java/com/aman/rest/user/User.java
@@ -0,0 +1,29 @@
+package com.aman.rest.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@RequiredArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank(message =  "username cannot be blank")
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message =  "password cannot be blank")
+    @NonNull
+    @Column(nullable = false)
+    private String password;
+
+}
