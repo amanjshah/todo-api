58411044035c441ce650b20a6f8953d383bf838d
Aman Shah <aman.j.shah@gmail.com>
2024-02-05 13:47:04 +0000

    save users to db upon registration

--- src/main/java/com/aman/rest/user/UserController.java
+++ src/main/java/com/aman/rest/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private CustomAuthenticationManager customAuthenticationManager;
+    private UserRepository userRepository;
 
     @PostMapping("/register")
     public User createUser(@Valid @RequestBody User user) {
@@ -29,6 +30,7 @@ public class UserController {
             )
         );
         customAuthenticationManager.addProvider(provider);
+        userRepository.save(user);
         return user;
     }
 }
