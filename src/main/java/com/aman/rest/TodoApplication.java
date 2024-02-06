2a0243fe83c5237278621407283722c5d1e292de
Aman Shah <aman.j.shah@gmail.com>
2024-02-06 12:51:26 +0000

    add dummy user with pre-configured to-do items in db (as defined in data.sql) upon service initialisation

--- src/main/java/com/aman/rest/TodoApplication.java
+++ src/main/java/com/aman/rest/TodoApplication.java
@@ -1,14 +1,24 @@
 package com.aman.rest;
 
+import com.aman.rest.user.User;
+import com.aman.rest.user.UserController;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+@AllArgsConstructor
+public class TodoApplication implements CommandLineRunner {
+
+	private UserController userController;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
-
+	@Override
+	public void run(String... args) {
+		userController.createUser(new User("aman", "dummy"));
+	}
 }
